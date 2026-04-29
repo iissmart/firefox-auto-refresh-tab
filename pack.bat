c3a6628e37c43d29c1bec374f5a61bf1dc41756f
Link, Ken <iissmart@numberzero.org>
2026-04-29 13:43:49 -0500

    Add SVG icons to packaging script for extension

--- pack.bat
+++ pack.bat
@@ -1,3 +1,3 @@
 @echo off
 del firefox-auto-refresh-tab.xpi 2>nul
-powershell -Command "Compress-Archive -Path 'background.js','LICENSE','manifest.json','popup.css','popup.html','popup.js','README.md' -DestinationPath 'firefox-auto-refresh-tab.zip' -Force; Rename-Item 'firefox-auto-refresh-tab.zip' 'firefox-auto-refresh-tab.xpi'"
+powershell -Command "Compress-Archive -Path 'background.js','icon48.svg','icon128.svg','LICENSE','manifest.json','popup.css','popup.html','popup.js','README.md' -DestinationPath 'firefox-auto-refresh-tab.zip' -Force; Rename-Item 'firefox-auto-refresh-tab.zip' 'firefox-auto-refresh-tab.xpi'"
